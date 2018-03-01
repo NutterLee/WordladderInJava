d5ccbb8b93d39c77e8137c1cd461619c2d27a527
NutterLee <Handong-Lee@sjtu.edu.cn>
2018-03-01 20:58:49 +0800

    重构了app
    重新生成了带gradle的版本 同时修复了bug
    bug记录：Java中拷贝stack应该用clone方法 （还要强制类型转换），之前一直直接赋值，导致实际上是一个stack

--- code/src/main/java/Main.java
+++ code/src/main/java/Main.java
@@ -1,7 +1,3 @@
-package com.company;
-
-
-
 import java.io.BufferedReader;
 import java.io.*;
 import java.io.FileReader;
@@ -10,14 +6,14 @@ import java.util.Scanner;
 import java.util.*;
 import java.util.concurrent.LinkedBlockingQueue;
 
-//bug record
-// unable to find any ladder
+
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-	// write your code here
+        // write your code here
         Set<String> dictionary = new HashSet<String>();
+        System.out.println(System.getProperty("user.dir"));
         String filename = "dictionary.txt";
         String word1;
         String word2;
@@ -26,48 +22,49 @@ public class Main {
         //File dic = new File("dictionary.txt");
         BufferedReader dic_reader= new BufferedReader(new FileReader(filename));
         String tmpword = null;
+
         while((tmpword=dic_reader.readLine())!=null)
         {
             dictionary.add(tmpword);
-        }
 
+        }
         //
-        Queue<Stack<String>> words = new LinkedBlockingQueue<Stack<String>>();
+        Queue<Stack<String>> words = new LinkedBlockingQueue<>();
         Stack<String> final_result = new Stack<>();
         Set<String> word_collection = new HashSet<>();
         //get word1 and word2
         System.out.println("Word #1 (or Enter to quit):");
         word1 = sc.nextLine();
-        if(word1=="")
+        if(word1.equals(""))
         {
             System.out.println("Have a nice day.");
             return;
         }
         System.out.println("Word #2 (or Enter to quit):");
         word2 = sc.nextLine();
-        if(word2=="")
+        if(word2.equals(""))
         {
             System.out.println("Have a nice day.");
             return;
         }
 
         //error handling
-        if(word1==word2 || dictionary.contains(word1)==false||dictionary.contains(word2)==false)
+        if(word1.equals(word2) || dictionary.contains(word1)==false||dictionary.contains(word2)==false)
         {
             System.out.println("word error");
             return;
         }
 
         //build the ladder
-        Stack<String> word_container = new Stack<String>();
+        Stack<String> word_container = new Stack<>();
         boolean get_result = false;
         word_container.push(word1);
-        words.add(word_container);
+        words.offer(word_container);
 
         while (!words.isEmpty())
         {
-            System.out.println("words cycle");
-            Stack<String> tmp_stack = words.remove();
+
+            Stack<String> tmp_stack = words.poll();
             String tmp_word = tmp_stack.peek();
 
             for (int i = 0; i < tmp_word.length() ; i++) {
@@ -75,7 +72,8 @@ public class Main {
                 {
                     String new_word = tmp_word.replace(tmp_word.charAt(i),character);
                     //System.out.println(new_word);
-                    if(new_word==word2)
+                    //System.out.println(tmp_word);
+                    if(word2.equals(new_word))
                     {
                         get_result=true;
                         tmp_stack.push(new_word);
@@ -85,11 +83,12 @@ public class Main {
                         }
                         break;
                     }
-                    if(!dictionary.contains(new_word)||new_word==tmp_word||word_collection.contains(new_word)) continue;
+                    if(!dictionary.contains(new_word)||new_word.equals(tmp_word)||word_collection.contains(new_word)) continue;
                     word_collection.add(new_word);
-                    Stack<String> tmp_stack2 = tmp_stack;
+                    //System.out.println(new_word);
+                    Stack<String> tmp_stack2 = (Stack<String>) tmp_stack.clone();
                     tmp_stack2.push(new_word);
-                    words.add(tmp_stack2);
+                    words.offer(tmp_stack2);
                 }
 
             }
